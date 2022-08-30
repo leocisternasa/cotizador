b7b052076983ff6e1706678b9fd19b008a466ef4
Leonardo Cisternas <leocisternasa@gmail.com>
2022-08-30 12:03:03 -0400

    Setup and launching cotizador project with React and Vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+import Header from './components/Header';
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hola Mundo, Main component</h1>
+      <Header />
+    </div>
+  )
+}
+
+export default App
